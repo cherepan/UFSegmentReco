74e51158499900d86ab73bbb50307d6d44bc4cdb
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2023-02-21 15:46:32 +0100

    add DataFormats

---
+++ DataFormats/CSCRecHit/src/classes.h
@@ -0,0 +1,16 @@
+#include <DataFormats/CSCRecHit/interface/CSCRecHit2D.h>
+#include <DataFormats/CSCRecHit/interface/CSCRecHit2DCollection.h>
+
+#include <DataFormats/CSCRecHit/interface/CSCSegment.h>
+#include <DataFormats/CSCRecHit/interface/CSCSegmentCollection.h>
+
+#include <DataFormats/CSCRecHit/interface/CSCWireHit.h>
+#include <DataFormats/CSCRecHit/interface/CSCWireHitCollection.h>
+
+#include <DataFormats/CSCRecHit/interface/CSCStripHit.h>
+#include <DataFormats/CSCRecHit/interface/CSCStripHitCollection.h>
+
+
+#include <DataFormats/Common/interface/Wrapper.h>
+#include <vector>
+
